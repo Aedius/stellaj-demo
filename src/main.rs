78ef677cdafc3a12332de11fea397be52245631a
Aedius <aedius.filmania@gmail.com>
2022-01-14 14:05:52 +0100

    replace actix by rocket

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
-use actix_web::{
-    web, App, Error, Responder,
-    HttpRequest, HttpServer, HttpResponse
-};
-use actix_web::web::Data;
-use eventstore::{All, Client, EventData, ReadResult};
-use futures::stream::TryStreamExt;
-use serde::{Serialize, Deserialize};
-use actix_web_actors::ws;
-use actix::{Actor, StreamHandler};
-use actix_files as fs;
-use std::time::{Duration, Instant};
-use actix::prelude::*;
+#[macro_use] extern crate rocket;
 
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(30);
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+use eventstore::{All, Client, EventData, ReadResult};
+use rocket::futures::TryStreamExt;
+use rocket::fs::{FileServer, relative};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::State;
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
 struct Greeting {
     name: String,
 }
 
-async fn greet(db: web::Data<Client>, req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
+struct DbState{
+    db: Client
+}
+
+#[get("/<name>")]
+async fn greet(db_state: &State<DbState>, name: &str) -> String {
+
+    let db = db_state.db.clone();
 
     let payload = Greeting {
         name: name.to_string(),
@@ -35,86 +33,9 @@ async fn greet(db: web::Data<Client>, req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
-/// Define HTTP actor
-struct MyWs{
-    heart_beat: Instant,
-}
-
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.heart_beat(ctx);
-    }
-}
 
 
-impl MyWs {
-    fn new(db: web::Data<Client>) -> Self {
-        Self {
-            heart_beat: Instant::now(),
-        }
-    }
-
-    /// helper method that sends ping to client every second.
-    ///
-    /// also this method checks heartbeats from client
-    fn heart_beat(&self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
-            if Instant::now().duration_since(act.heart_beat) > CLIENT_TIMEOUT {
-                // heartbeat timed out
-                println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
-                ctx.stop();
-
-                // don't try to send a ping
-                return;
-            }
-
-            ctx.ping(b"");
-        });
-    }
-
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => {
-                self.heart_beat = Instant::now();
-                ctx.pong(&msg)
-            },
-            Ok(ws::Message::Pong(_)) => {
-                self.heart_beat = Instant::now();
-            }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => {
-                ctx.close(reason);
-                ctx.stop();
-            }
-            _ => ctx.stop(),
-        }
-    }
-}
-
-async fn ws(db: web::Data<Client>,req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", req);
-    let resp = ws::start(MyWs::new(db), &req, stream);
-    println!("{:?}", resp);
-    resp
-}
-
-
-#[actix_web::main]
+#[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Creates a client settings for a single node configuration.
@@ -136,16 +57,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(client.clone()))
-            .route("/ws", web::get().to(ws))
-            .route("/greeting/", web::get().to(greet))
-            .route("/greeting/{name}", web::get().to(greet))
-            .service(fs::Files::new("/", "./static").index_file("index.html"))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
+    rocket::build()
+        .manage(DbState{ db: client.clone()})
+        .mount("/hello", routes![greet])
+        .mount("/", FileServer::from(relative!("static")))
+        .launch()
         .await?;
 
     Ok(())
