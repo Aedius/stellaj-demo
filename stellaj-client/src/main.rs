b23bacebff828a026e08ef402a6f24e124ca422a
Aedius <aedius.filmania@gmail.com>
2022-01-22 01:15:34 +0100

    cargo fmt

--- stellaj-client/src/main.rs
+++ stellaj-client/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fmt::{Debug}};
+use std::fmt::Debug;
 
 use yew::prelude::*;
 
@@ -6,9 +6,8 @@ use gloo_events::EventListener;
 use reqwasm::http::Request;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::spawn_local;
-use web_sys::{Event, EventSource, MessageEvent, window};
+use web_sys::{window, Event, EventSource, MessageEvent};
 use yew::{html, Component, Context, Html};
-
 
 #[macro_use]
 extern crate serde_derive;
@@ -29,25 +28,24 @@ pub struct Tokens {
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 struct Wc {
-    name: String
+    name: String,
 }
 
-enum WelcomeMsg{
-    EsReady(Result<Wc, serde_json::Error>)
+enum WelcomeMsg {
+    EsReady(Result<Wc, serde_json::Error>),
 }
 
-struct WelcomeComp{
+struct WelcomeComp {
     es: EventSource,
     member: Vec<String>,
     _listener: EventListener,
 }
 
-impl Component for WelcomeComp{
+impl Component for WelcomeComp {
     type Message = WelcomeMsg;
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-
         let es = EventSource::new("/event/greetings")
             .map_err(|js_value: JsValue| {
                 let err: js_sys::Error = js_value.dyn_into().unwrap();
@@ -56,9 +54,9 @@ impl Component for WelcomeComp{
             })
             .unwrap();
 
-        let cb = ctx.link().callback(|bufstr: String| {
-            WelcomeMsg::EsReady(serde_json::from_str(&bufstr))
-        });
+        let cb = ctx
+            .link()
+            .callback(|bufstr: String| WelcomeMsg::EsReady(serde_json::from_str(&bufstr)));
         let listener = EventListener::new(&es, "message", move |event: &Event| {
             let event = event.dyn_ref::<MessageEvent>().unwrap();
             let text = event.data().as_string().unwrap();
@@ -66,15 +64,14 @@ impl Component for WelcomeComp{
             cb.emit(text);
         });
 
-        WelcomeComp{
+        WelcomeComp {
             es,
-            member: vec!("test1".to_string(),"test2".to_string()),
-            _listener: listener
+            member: vec!["test1".to_string(), "test2".to_string()],
+            _listener: listener,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-
         match msg {
             WelcomeMsg::EsReady(response) => {
                 match response {
@@ -89,7 +86,6 @@ impl Component for WelcomeComp{
         }
         true
     }
-
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
@@ -107,29 +103,32 @@ impl Component for WelcomeComp{
 
 #[function_component(App)]
 fn app() -> Html {
-
     let window = window().unwrap();
     let location = window.location();
     let params = location.search().unwrap();
 
-    if !params.is_empty(){
+    if !params.is_empty() {
         log::info!("location : {:?}", params);
         spawn_local(async move {
             let resp = Request::post(format!("/auth/login-token{}", params).as_str())
-                .send().await.unwrap();
+                .send()
+                .await
+                .unwrap();
 
-            let token =  resp.json::<Tokens>().await.unwrap();
-            log::info!("tokens : {:?}",token);
+            let token = resp.json::<Tokens>().await.unwrap();
+            log::info!("tokens : {:?}", token);
 
             let message = Request::get("/auth/welcome")
-                .header("Authorization", format!("Bearer {}", token.access_token).as_str())
-                .send().await.unwrap();
-
-
+                .header(
+                    "Authorization",
+                    format!("Bearer {}", token.access_token).as_str(),
+                )
+                .send()
+                .await
+                .unwrap();
 
             log::info!("message : {:?}", message.text().await.unwrap());
         });
-
     }
 
     html! {
--- stellaj-server/src/auth.rs
+++ stellaj-server/src/auth.rs
@@ -1,18 +1,14 @@
-
-
 use jsonwebtokens as jwt;
-use jwt::{Algorithm, AlgorithmID, Verifier, raw};
+use jwt::{Algorithm, AlgorithmID, Verifier};
 use reqwest::header::HeaderMap;
 use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::response::Redirect;
 use rocket::serde::json::serde_json::from_str;
 use rocket::serde::{json::Json, Deserialize, Serialize};
-use rocket::request::{self, Outcome, Request, FromRequest};
-use rocket::{Error, Route};
-use serde_json::json;
+use rocket::Route;
 use serde_json::value::Value;
 use urlencoding::encode;
-
 
 pub fn get_route() -> Vec<Route> {
     return routes![login, login_token, welcome];
@@ -84,8 +80,8 @@ pub async fn login_token(session_state: &str, code: &str) -> Json<Tokens> {
 }
 
 #[derive(Debug)]
-pub struct KeyCloakUser{
-    pub username: String
+pub struct KeyCloakUser {
+    pub username: String,
 }
 
 #[derive(Debug)]
@@ -96,8 +92,8 @@ pub enum KeyCloakError {
 
 #[derive(Debug, Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct Claims{
-    preferred_username: String
+struct Claims {
+    preferred_username: String,
 }
 
 #[rocket::async_trait]
@@ -114,30 +110,30 @@ impl<'r> FromRequest<'r> for KeyCloakUser {
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5rLHfJBzm3Z+abdmeDNYSvwqWou5TGbyqi4JZJ7PcL90gGI8ZGAIbmpCntSTzB6mtaaccpkYPBv3E7zbEmWxrOf/Y+8Wutmc2V4fDEWivSMbsUwOJBhOaRY0mkaKzrX1b4jgex6Fbox9RiJrr2g1SXgncVBuKhbe1rwFla+LpTMMxK7uvJRiXyOSMFAq51d0qbQVzwz+qF4oQ5nqn2WWuI+ZER+Pog5afVpJOIs1gDKNIMSCMq64q3WwFXqTiJZaYTq6ap2KoqoNy8euwRVhxMzt47j/EdZIYC6iruzAOTWl6JKfsKH8uNTQ1v5BsTXmv7EwKf1bK+LJQEJziwLAqQIDAQAB
 -----END PUBLIC KEY-----";
 
-                let mut alg = Algorithm::new_rsa_pem_verifier(AlgorithmID::RS256, sig.as_bytes()).unwrap();
+                let mut alg =
+                    Algorithm::new_rsa_pem_verifier(AlgorithmID::RS256, sig.as_bytes()).unwrap();
                 alg.set_kid("OMilXIowKglmPO1ATFfTPxzP9QwOIFgV-Ya_Ntx4dsw");
                 let verifier = Verifier::create()
                     .issuer("http://localhost:8080/auth/realms/horfimbor")
                     .audience("account")
                     .ignore_iat()
-                    .build().unwrap();
+                    .build()
+                    .unwrap();
 
                 let claims: Value = verifier.verify(&key, &alg).unwrap();
 
-                let claims:Claims = serde_json::from_value(claims).unwrap();
+                let claims: Claims = serde_json::from_value(claims).unwrap();
 
                 info!("claims : {:?}", claims);
 
-                Outcome::Success(KeyCloakUser{
-                    username: claims.preferred_username
+                Outcome::Success(KeyCloakUser {
+                    username: claims.preferred_username,
                 })
-            },
-            Some(_) => Outcome::Failure((Status::BadRequest, KeyCloakError::Missing)),
+            }
+            Some(_) => Outcome::Failure((Status::BadRequest, KeyCloakError::Invalid)),
         }
-
     }
 }
-
 
 #[get("/welcome")]
 pub fn welcome(user: KeyCloakUser) -> String {
