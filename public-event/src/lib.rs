a8957c34c669753b4bd898b6095fdb3994841326
Aedius <aedius.filmania@gmail.com>
2022-02-07 22:22:07 +0100

    create crate

--- public-event/src/lib.rs
+++ public-event/src/lib.rs
@@ -1,9 +1,11 @@
+use serde::{Serialize, Deserialize};
 
-
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize )]
 pub enum HomepageEvent{
     NewPlayer(String),
 }
 
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize )]
 pub struct HomepageState{
     pub nb_player: usize,
     pub last_players: Vec<String>,
